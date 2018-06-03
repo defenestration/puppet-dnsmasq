36bad06bab80c8cecf9eb22558f4f91dd5206dd7
Alan Brevick <mrmincho@gmail.com>
2018-06-03 00:09:25 -0400

    use hash

--- manifests/dhcpoption.pp
+++ manifests/dhcpoption.pp
@@ -1,9 +1,20 @@
 # Create an dnsmasq dhcp option (--dhcp-option).
 class dnsmasq::dhcpoption (
+  Hash $hash = undef,
   $option = undef ,
   $content = undef ,
   $tag = undef,
 ) {
+
+  if not $hash = undef {
+    hash.each | $option, $content  | {
+      concat::fragment { "dnsmasq-dhcpoption-${option}":
+        order   => '03',
+        target  => 'dnsmasq.conf',
+        content => template('dnsmasq/dhcpoption.erb'),
+      }
+    }
+  }
   $tag_real = $tag ? {
     undef   => '',
     default => "tag:${tag},",
@@ -11,9 +22,11 @@ class dnsmasq::dhcpoption (
 
   include dnsmasq
 
-  concat::fragment { "dnsmasq-dhcpoption-${name}":
+  concat::fragment { "dnsmasq-dhcpoption-${option}":
     order   => '03',
     target  => 'dnsmasq.conf',
+    content => template('dnsmasq/dhcpoption.erb'),
+  }
     content => template('dnsmasq/dhcpoption.erb'),
   }
 
